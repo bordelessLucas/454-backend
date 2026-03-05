d7963f82060bb28fe41619728914d871448b1086
luanpaacheco <luan.005@edu.pucrs.br>
2026-03-05 02:21:55 -0300

    tentando fazer o render injetar as variaveis

--- hello-prisma/prisma.config.ts
+++ hello-prisma/prisma.config.ts
@@ -2,9 +2,14 @@
 // npm install --save-dev prisma dotenv
 import { defineConfig } from "prisma/config";
 
+const databaseUrl = process.env["DATABASE_URL"];
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+  },
+  datasource: {
+    url: databaseUrl,
   },
 });
